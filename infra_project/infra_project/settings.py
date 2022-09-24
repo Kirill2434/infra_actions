ae2a5e6a48726a892b5cdefb6a55dd770886b262
grish <cool.yn20@yandex.ru>
2022-09-24 23:05:46 +0300

    окей

--- infra_project/infra_project/settings.py
+++ infra_project/infra_project/settings.py
@@ -48,7 +48,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'debug_toolbar.middleware.DebugToolbarMiddleware'
+    # 'debug_toolbar.middleware.DebugToolbarMiddleware'
 ]
 
 ROOT_URLCONF = 'infra_project.urls'
